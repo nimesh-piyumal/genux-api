9a70cbb1d283e8a5d4d26216adf9d266b1e4e9b0
Nimesh Piyumal <nimeshofficial.info@gmail.com>
2025-04-02 13:53:00 +0000

    genux-api

---
+++ app/api/hello/route.js
@@ -0,0 +1,8 @@
+export async function GET(request) {
+    return new Response(JSON.stringify({ message: 'Hello from Next.js API!' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+}
